d2526f3692cd930f1df71a35ce68d264a00ad2ad
Aayush Rohatgi <aayushrohatgi07@gmail.com>
2017-05-09 18:55:19 +0530

    commit-4

--- Dynamic Programs/src/Launcher.java
+++ Dynamic Programs/src/Launcher.java
@@ -6,17 +6,17 @@ public class Launcher {
 
 	public static void main(String[] args) {
 
-		int number = 1090545;
+		int number = 1091545;
 
 		PossibleCombinations combinations = new PossibleCombinations();
 		long startTime = System.currentTimeMillis();
 		List<Integer> results = new ArrayList<>(combinations.generateConbinations(number));
 		long endTime = System.currentTimeMillis();
 		System.out.println("Took " + (endTime - startTime) + " ns");
-		System.out.println(results.size());
 		Collections.sort(results);
 		for (int result : results) {
 			System.out.println(result);
 		}
+		System.out.println(results.size());
 	}
 }
